132e17983e3742522130643db820988fbf1896ce
weeksjc13 <31712223+weeksjc13@users.noreply.github.com>
2017-10-03 17:56:13 -0400

    Changed some file names
    Now you can see what class the code in the files associate with.

--- Legacy/Program 2 copyyy/MemParam.java
+++
@@ -1,6 +0,0 @@
-public class MemParam {
-  public static final int NUM_PAGES = 30;
-  public static final int NUM_PROGRAMS = 20;
-  public static final int MAX_SIZE = 25;
-  public static final int PAGE_SIZE = 3;
-}
